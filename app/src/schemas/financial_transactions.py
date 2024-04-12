6bb2f62ae4187078573e5de1d4df9201db9d9bbf
FTKV <mthkvv@gmail.com>
2024-04-12 02:39:04 +0100

    fin_trans

---
+++ app/src/schemas/financial_transactions.py
@@ -0,0 +1,40 @@
+"""
+Module of financial transactions' schemas
+"""
+
+from decimal import Decimal
+from datetime import datetime
+import enum
+from typing import Annotated, Optional
+
+from sqlalchemy import Enum, Numeric
+from pydantic import (
+    BaseModel,
+    model_validator,
+    Field,
+    ConfigDict,
+    UUID4,
+)
+
+from src.database.models import TrxType
+
+
+class FinancialTransactionModel(BaseModel):
+    trx_type: TrxType
+    debit: float
+    credit: float
+    user_id: UUID4 | int
+    reservation_id: UUID4 | int
+
+    @model_validator(mode="before")
+    def round_debit_credit(cls, v):
+        trx_type = v.get("trx_type")
+        debit = v.get("debit")
+        credit = v.get("credit")
+        if trx_type == TrxType.CHARGE.value and credit != 0:
+            raise ValueError(f"Charge transaction shouldn't has credit != 0")
+        if trx_type == TrxType.PAYMENT.value and debit != 0:
+            raise ValueError(f"Payment transaction shouldn't has debit != 0")
+        v["debit"] = round(debit, 2)
+        v["credit"] = round(credit, 2)
+        return v
