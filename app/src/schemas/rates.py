223722e43c92e323b2890b7dfb9ae795eca2cac8
Hellboy <136455069+Victor3637@users.noreply.github.com>
2024-04-14 18:33:33 +0300

    Update rates.py

--- app/src/schemas/rates.py
+++ app/src/schemas/rates.py
@@ -2,25 +2,25 @@
 Module of rates' schemas
 """
 
-from datetime import datetime
 from typing import List
+from pydantic import BaseModel, Field, UUID4, conlist
 
-from pydantic import BaseModel, Field, UUID4
+class RateDetail(BaseModel):
+    id: UUID4
+    rate_id: UUID4
+    detail: str
 
-from .rate_details import RateDetailModel
-
-class RateModel(BaseModel):
-    title: str = Field(min_length=1, max_length=32)
-    description: str
-    start_date: datetime
-    end_date: datetime
-    hourly_rate: int
-    daily_rate: int
-
-    rate_details: List[RateDetailModel] = []
+class Rate(BaseModel):
+    id: UUID4
+    title: str = Field(..., max_length=32)
+    description: Optional[str] = Field(None, max_length=1024)
+    is_daily: bool
+    user_id: UUID4
+    rate_details: List[RateDetail] = []
 
     class Config:
         orm_mode = True
+
 
 class RateResponse(BaseModel):
     id: UUID4 | int
