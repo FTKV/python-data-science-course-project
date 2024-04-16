02b1be77eb3eb2e570290a06fefd844405999d79
Hellboy <136455069+Victor3637@users.noreply.github.com>
2024-04-16 15:06:42 +0300

    Update rates.py

--- app/src/routes/rates.py
+++ app/src/routes/rates.py
@@ -32,8 +32,8 @@ async def create_rate(
     """
     Handles a POST-operation to create a rate.
 
-    :param rate_input: The data for the rate to create.
-    :type rate_input: RateInput
+    :param rate: The data for the rate to create.
+    :type rate: RateBase
     :param session: The database session.
     :type session: AsyncSession
     :return: The newly created rate.
@@ -133,8 +133,10 @@ async def add_rate_detail_to_rate(
     """
     Handles a POST-operation to create a rate detail.
 
+    :param rate_id: The ID of the rate to which the rate detail will be added.
+    :type rate_id: Union[UUID4, int]
     :param rate_detail_input: The data for the rate detail to create.
-    :type rate_detail_input: RateDetailInput
+    :type rate_detail_input: RateDetailModel
     :param session: The database session.
     :type session: AsyncSession
     :return: The newly created rate detail.
@@ -143,4 +145,102 @@ async def add_rate_detail_to_rate(
     rate_detail = await repository_rate_details.create_rate_detail(
         rate_id, rate_detail_input, user, session
     )
+    return rate_detail
+
+
+@router.get(
+    "/{rate_id}/rate_details",
+    response_model=List[RateDetailResponse],
+    dependencies=[Depends(allowed_operations_for_all)],
+)
+async def read_rate_details_for_rate(
+    rate_id: UUID4 | int,
+    offset: int = 0,
+    limit: int = 10,
+    session: AsyncSession = Depends(get_session),
+):
+    """
+    Handles a GET-operation to get all rate details for a rate.
+
+    :param rate_id: The ID of the rate for which rate details will be fetched.
+    :type rate_id: Union[UUID4, int]
+    :param offset: The number of rate details to skip.
+    :type offset: int
+    :param limit: The maximum number of rate details to return.
+    :type limit: int
+    :param session: The database session.
+    :type session: AsyncSession
+    :return: The list of rate details for the specified rate.
+    :rtype: List[RateDetailResponse]
+    """
+    rate_details = await repository_rate_details.read_rate_details_for_rate(
+        rate_id, offset, limit, session
+    )
+    return rate_details
+
+
+@router.put(
+    "/{rate_id}/rate_details/{rate_detail_id}",
+    response_model=RateDetailResponse,
+    dependencies=[Depends(allowed_operations_for_all)],
+)
+async def update_rate_detail_for_rate(
+    rate_id: UUID4 | int,
+    rate_detail_id: int,
+    rate_detail_update: RateDetailUpdate,
+    session: AsyncSession = Depends(get_session),
+):
+    """
+    Handles a PUT-operation to update a rate detail for a rate.
+
+    :param rate_id: The ID of the rate for which the rate detail will be updated.
+    :type rate_id: Union[UUID4, int]
+    :param rate_detail_id: The ID of the rate detail to update.
+    :type rate_detail_id: int
+    :param rate_detail_update: The updated data for the rate detail.
+    :type rate_detail_update: RateDetailUpdate
+    :param session: The database session.
+    :type session: AsyncSession
+    :return: The updated rate detail.
+    :rtype: RateDetailResponse
+    """
+    rate_detail = await repository_rate_details.update_rate_detail_for_rate(
+        rate_id, rate_detail_id, rate_detail_update, session
+    )
+    if not rate_detail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Rate detail not found"
+        )
+    return rate_detail
+
+
+@router.delete(
+    "/{rate_id}/rate_details/{rate_detail_id}",
+    response_model=RateDetailResponse,
+    dependencies=[Depends(allowed_operations_for_all)],
+)
+async def delete_rate_detail_for_rate(
+    rate_id: UUID4 | int,
+    rate_detail_id: int,
+    session: AsyncSession = Depends(get_session),
+):
+    """
+    Handles a DELETE-operation to delete a rate detail for a rate.
+
+    :param rate_id: The ID of the rate for which the rate detail will be deleted.
+    :type rate_id: Union[UUID4, int]
+    :param rate_detail_id: The ID of the rate detail to delete.
+    :type rate_detail_id: int
+    :param session: The database session.
+    :type session: AsyncSession
+    :return: The deleted rate detail.
+    :rtype: RateDetailResponse
+    """
+    rate_detail = await repository_rate_details.delete_rate_detail_for_rate(
+        rate_id, rate_detail_id, session
+    )
+    if not rate_detail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Rate detail not found"
+        )
     return rate_detail
